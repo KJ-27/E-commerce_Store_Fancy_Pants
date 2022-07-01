f3051f7e909b63258e843df4577d7709804f62c8
KJ_27 <cespedeskendall@hotmail.com>
2022-06-30 18:54:26 -0600

    View and navigate

--- app/src/main/java/com/example/e_commerce_store_fancy_pants/ui/producto/ProductoFragment.kt
+++ app/src/main/java/com/example/e_commerce_store_fancy_pants/ui/producto/ProductoFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.e_commerce_store_fancy_pants.R
 import com.example.e_commerce_store_fancy_pants.databinding.FragmentProductoBinding
 import com.example.e_commerce_store_fancy_pants.viewmodel.ProductoViewModel
 
@@ -22,6 +24,10 @@ class ProductoFragment : Fragment() {
     ): View {
         productoViewModel = ViewModelProvider(this)[ProductoViewModel::class.java]
         _binding = FragmentProductoBinding.inflate(inflater, container, false)
+
+        binding.addProductoFabButton.setOnClickListener{
+            findNavController().navigate(R.id.action_nav_producto_to_addProductoFragment)
+        }
 
         return binding.root
     }
