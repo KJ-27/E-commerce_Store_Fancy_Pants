9e8ebdabf4a00a49b44475cc44f2f2240fe897cc
KJ_27 <cespedeskendall@hotmail.com>
2022-06-30 21:14:00 -0600

    Read Done
    Ya se ven

--- app/src/main/java/com/example/e_commerce_store_fancy_pants/ui/producto/ProductoFragment.kt
+++ app/src/main/java/com/example/e_commerce_store_fancy_pants/ui/producto/ProductoFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.e_commerce_store_fancy_pants.R
+import com.example.e_commerce_store_fancy_pants.adapter.ProductoAdapter
 import com.example.e_commerce_store_fancy_pants.databinding.FragmentProductoBinding
 import com.example.e_commerce_store_fancy_pants.viewmodel.ProductoViewModel
 
@@ -27,6 +30,17 @@ class ProductoFragment : Fragment() {
 
         binding.addProductoFabButton.setOnClickListener{
             findNavController().navigate(R.id.action_nav_producto_to_addProductoFragment)
+        }
+
+        val productoAdapter = ProductoAdapter()
+        val reciclador = binding.reciclador
+        reciclador.adapter = productoAdapter
+        reciclador.layoutManager = LinearLayoutManager(requireContext())
+
+        productoViewModel = ViewModelProvider(this)[ProductoViewModel::class.java]
+
+        productoViewModel.getAllData.observe(viewLifecycleOwner) { producto ->
+            productoAdapter.setData(producto)
         }
 
         return binding.root
